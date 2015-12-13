fd26ec9e2379e4272ee2d5917667e21aa46ec9e2
wahyudinata setiawan <wahyudinata@gmail.com>
2015-12-13 14:02:09 -0800

    init comment, the whole kitchensink

---
+++ kitchensink/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){ // classic and nostalgic
+    //you can use any of the javascript frameworks; they are like snowflakes, as in, they are a lot and they melt away fast
+    UnoPico.ready(function($userv1, $appv1, $statev1){
+        /**
+         * you can inject these modules as function parameters as you need it, right now there are only those listed above.
+         * There will be more modules to be added, such as $realtimev1 (for real time communication such as chat) or
+         * maybe $taskv1 for background tasks? Ugh the possibilities.
+         * If you have ideas, please do join our Slack chat, would love to hear :)
+         */
+    })
+})
